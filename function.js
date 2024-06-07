705b4fb8211c0f447617e4ea9d97e4ffdfb69af1
Jyoti657 <mouryajyoti657@gmail.com>
2024-06-07 11:01:46 +0000

    done

---
+++ function.js
@@ -0,0 +1,60 @@
+//  function hi(){
+//     console.log("j");
+//     console.log("y");
+//     console.log("j");
+//     console.log("o");
+//     console.log("j");
+//     console.log("t");
+//     console.log("i");
+
+
+//  }
+//  hi();
+// function sum(num1,num2){
+//     console.log(num1+num2);
+
+// }
+// sum(2,4);
+// function sum(num1,num2){
+//     let res=num1+num2
+//     return res;
+
+// }
+//  const res=sum(2,4)
+// console.log("resut",res);
+
+// different function method
+
+// function msg(username){
+//     if(username===undefined){
+//         console.log("plz enter your msg");
+//         return
+//     }
+//      return`${username} just login `
+// }
+// console.log(msg("hello"));
+
+// function with many parmar
+
+// function calcucart(...ret1){
+//     return ret1 
+    
+// }
+// console.log(calcucart(200,3546,788,));
+
+// now obj and function connection
+
+
+const username={
+    nam:"janvi",
+    age:11,
+
+}
+function objfu(anyobj){
+    console.log(`username is ${anyobj.nam} price is ${anyobj.age}`);
+
+
+}
+objfu(username)
+
+
